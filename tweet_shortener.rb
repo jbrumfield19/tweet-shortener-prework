0f72f4713e4fd50b0b84d4f6dc7be5b35d38b9f2
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-05 00:55:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,3 +15,25 @@ def word_substituter(tweet)
   result = og_words.join(" ")
   result
 end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+ def selective_tweet_shortener(tweet)
+  (tweet.size > 140)? word_substituter(tweet) : tweet
+end
+
+ def shortened_tweet_truncator(tweet)
+  shortened_tweet = selective_tweet_shortener(tweet)
+  truncated_tweet = ""
+  if shortened_tweet.size>140
+    truncated_tweet = shortened_tweet[0..139]
+  else
+    truncated_tweet = shortened_tweet
+  end
+  truncated_tweet
+
+ end
