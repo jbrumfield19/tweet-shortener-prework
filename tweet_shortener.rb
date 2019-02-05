c9b00e9f9d01c1659f61c097ba299c971be90d00
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-05 00:53:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary 
-  dictionary = {"hello" => "hi", "to"=>'2', 'two'=>'2', "too" => '2', 'for'=>'4','four' => '4', 'you' => 'u', 'at' => '@', 'and' => '&'}
+  dictionary = {"hello" => "hi", "to"=>'2', 'two'=>'2', "too" => '2', 'for'=>'4','four' => '4', 'you' => 'u', 'at' => '@', 'and' => '&','be'=>'b'}
 end
 def word_substituter(tweet_one)
    substitutes = dictionary
