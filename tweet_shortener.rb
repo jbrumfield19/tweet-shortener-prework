e2d701638771e4e986a139f4ada7916bc02a39cc
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-05 00:46:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def dictionary 
+  dictionary = {"hello" => "hi", "to, two, too" => '2', 'for,four' => '4', 'you' => 'u', 'at' => '@', 'and' => '&'}
+end
+def word_substituter(tweet_one)
+   substitutes = dictionary
+  og_words = tweet_one.split
+    og_words.collect! do |word|
+      lowercased = word.downcase
+      if substitutes.keys.include? lowercased
+        substitutes[lowercased]
+      else
+        word
+      end
+    end
+  result = og_words.join(" ")
+  result
+end
